8c078fcb340cf73845561617141bbd377fe6acb0
ConceptualCode <soronnadianthony@gmail.com>
2023-12-17 19:22:04 +0100

    Improved the code to be more modular by writing different classes for DocumentLoader and DocumentSplitter

--- utils.py
+++ utils.py
@@ -1,37 +1,29 @@
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+from langchain.document_loaders import PyPDFLoader, YoutubeAudioLoader, WebBaseLoader
 from langchain.document_loaders.parsers import OpenAIWhisperParser
 from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-class DocumentProcessor:
-    def __init__(self, chunk_size=100, chunk_overlap=20):
-        """
-        Initializes the DocumentProcessor with a RecursiveTextSplitter.
 
-        Args:
-        chunk_size (int): Maximum size of each text chunk.
-        chunk_overlap (int): Number of characters of overlap between chunks.
+class DocumentLoader:
+    def __init__(self):
         """
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        Initializes the DocumentLoader.
+        """
+        pass  # No initialization needed for simple loading
 
-    def load_and_split_pdf(self, file_path):
+    def load_pdf(self, file_path):
         """
-        Loads a PDF file using langchain's PyPDFLoader and splits it into chunks using RecursiveTextSplitter.
+        Loads a PDF file using langchain's PyPDFLoader.
 
         Args:
         file_path (str): Path to the PDF file.
 
         Returns:
-        list: A list of text chunks from the PDF file.
+        list: A list of Documents from the PDF file.
         """
         loader = PyPDFLoader(file_path)
-        documents = loader.load() 
-
-        # Split the document into chunks
-        chunks = self.text_splitter.split_documents(documents)
-        return chunks
+        documents = loader.load()
+        return documents
     
     def load_from_youtube(self, video_url, save_dir='youtube'):
         """
@@ -45,7 +37,7 @@ class DocumentProcessor:
         str: Transcribed text content from the YouTube video.
         """
         youtube_loader = YoutubeAudioLoader([video_url], save_dir)
-        parser = OpenAIWhisperParser() 
+        parser = OpenAIWhisperParser()
         loader = GenericLoader(youtube_loader, parser)
         docs = loader.load()
         return ' '.join([doc.page_content for doc in docs])
@@ -63,3 +55,28 @@ class DocumentProcessor:
         web_loader = WebBaseLoader(web_path=url)
         text = web_loader.load()
         return text
+
+class DocumentSplitter:
+    def __init__(self, chunk_size=500, chunk_overlap=50, separators=["\n\n", "\n", ". ", " ", ""]):
+        """
+        Initializes the DocumentSplitter with a RecursiveTextSplitter.
+
+        Args:
+        chunk_size (int): Maximum size of each text chunk.
+        chunk_overlap (int): Number of characters of overlap between chunks.
+        """
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, 
+                                                            chunk_overlap=chunk_overlap, 
+                                                            separators=separators)
+
+    def split_document(self, document):
+        """
+        Splits the document into chunks using RecursiveTextSplitter.
+
+        Args:
+        document (str): The document to be split.
+
+        Returns:
+        list: A list of text chunks.
+        """
+        return self.text_splitter.split_text(document)
